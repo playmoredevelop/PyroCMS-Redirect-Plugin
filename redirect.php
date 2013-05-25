c3c9682f5a4acf39b85b805c497305609b04af6b
Playmore <playmoredevelop@gmail.com>
2013-05-25 16:09:27 +0400

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -7,85 +7,91 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  *
  * Redirect with status code.
  *
- * @author  	Playmore Dev
+ * @author		PyroCMS Dev Team
  * @package		PyroCMS\Addon\Plugins
- * @copyright	Copyright (c) 2013 Playmore
+ * @copyright	Copyright (c) 2009 - 2010, PyroCMS
  */
 class Plugin_Redirect extends Plugin {
 
     public $version = '1.0.0';
     public $name = array(
-	'en' => 'Redirects',
+	'en' => 'Redirect',
 	'ru' => 'Редиректы'
     );
     public $description = array(
-	'en' => 'Redirects with status code',
-	'ru' => 'Редиректы с кодом перенаправления'
+	'en' => 'With',
+	'ru' => 'Редиректы с кодом.'
     );
     private $_url = NULL;
+
+    /**
+     *
+     * @var CI_Uri
+     */
+    public $uri;
 
     public function _self_doc() {
 	$info = array(
 	    'refresh' => array(
-		'description' => array(
+		'description' => array(// a single sentence to explain the purpose of this method
 		    'en' => 'Redirect to url',
 		    'ru' => ''
 		),
-		'single' => true, 
-		'double' => false, 
-		'variables' => 'url', 
+		'single' => true, // will it work as a single tag?
+		'double' => false, // how about as a double tag?
+		'variables' => 'url', // list all variables available inside the double tag. Separate them|like|this
 		'attributes' => array(
-		    'url' => array(
-			'type' => 'text', 
-			'flags' => '', 
-			'default' => '', 
-			'required' => false, 
+		    'url' => array(// this is the name="World" attribute
+			'type' => 'text', // Can be: slug, number, flag, text, array, any.
+			'flags' => '', // flags are predefined values like asc|desc|random.
+			'default' => '', // this attribute defaults to this if no value is given
+			'required' => false, // is this attribute required?
 		    ),
 		),
 	    ),
 	    'w301' => array(
-		'description' => array(
+		'description' => array(// a single sentence to explain the purpose of this method
 		    'en' => '301 Moved Permanently if page not found. Add a tag to your page with an error 404',
 		    'ru' => ''
 		),
-		'single' => true, 
-		'double' => false, 
-		'variables' => 'if|url', 
+		'single' => true, // will it work as a single tag?
+		'double' => false, // how about as a double tag?
+		'variables' => 'if|url', // list all variables available inside the double tag. Separate them|like|this
 		'attributes' => array(
-		    'if' => array(
-			'type' => 'text', 
-			'flags' => '', 
-			'default' => '',
-			'required' => true,
+		    'if' => array(// this is the name="World" attribute
+			'type' => 'text', // Can be: slug, number, flag, text, array, any.
+			'flags' => '', // flags are predefined values like asc|desc|random.
+			'default' => '', // this attribute defaults to this if no value is given
+			'required' => true, // is this attribute required?
 		    ),
-		    'url' => array(
-			'type' => 'text',
-			'flags' => '',
-			'default' => '',
-			'required' => true,
+		    'url' => array(// this is the name="World" attribute
+			'type' => 'text', // Can be: slug, number, flag, text, array, any.
+			'flags' => '', // flags are predefined values like asc|desc|random.
+			'default' => '', // this attribute defaults to this if no value is given
+			'required' => true, // is this attribute required?
 		    ),
 		),
 	    ),
 	    'with' => array(
-		'description' => array(
+		'description' => array(// a single sentence to explain the purpose of this method
 		    'en' => 'Manually specify the code to redirect',
 		    'ru' => ''
 		),
-		'single' => true,
-		'double' => false,
-		'variables' => 'code|url',
+		'single' => true, // will it work as a single tag?
+		'double' => false, // how about as a double tag?
+		'variables' => 'code|url', // list all variables available inside the double tag. Separate them|like|this
 		'attributes' => array(
-		    'code' => array(
-			'type' => 'text',
-			'flags' => '',
-			'default' => '',
-			'required' => true,
+		    'code' => array(// this is the name="World" attribute
+			'type' => 'text', // Can be: slug, number, flag, text, array, any.
+			'flags' => '', // flags are predefined values like asc|desc|random.
+			'default' => '', // this attribute defaults to this if no value is given
+			'required' => true, // is this attribute required?
 		    ),
-		    'url' => array(
-			'type' => 'text',
-			'flags' => '',
-			'default' => '',
-			'required' => true,
+		    'url' => array(// this is the name="World" attribute
+			'type' => 'text', // Can be: slug, number, flag, text, array, any.
+			'flags' => '', // flags are predefined values like asc|desc|random.
+			'default' => '', // this attribute defaults to this if no value is given
+			'required' => true, // is this attribute required?
 		    ),
 		),
 	    ),
@@ -106,13 +112,13 @@ class Plugin_Redirect extends Plugin {
 
     /*
      * 301 Moved Permanently
-     * {{ redirect:w301 if="_url404_" url="_redirect_" }}
+     * {{ redirect:moved if="_url404_" url="_redirect_" }}
      */
-    public function w301() {
+    public function moved() {
 
 	if ($url404 = $this->attribute('if', FALSE) AND $url404 == $_SERVER['REQUEST_URI']) {
 
-		$this->_init();
+	    $this->_init();
 	    return redirect($this->_url, 'auto', 301);
 	}
     }
@@ -133,3 +139,5 @@ class Plugin_Redirect extends Plugin {
     }
 
 }
+
+/* End of file example.php */
